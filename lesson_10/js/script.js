8e90d2d72f005fb53066ed7b59f9bcca261eee8e
Maksym Kotsupyr <maksym.kotsupyr@gmail.com>
2020-04-04 00:16:38 +0300

    symbol iterator, for of

---
+++ lesson_10/js/script.js
@@ -0,0 +1,37 @@
+function* numGen(n) {
+    for (let i=0; i<n; i++){
+        yield i
+    }
+}
+const num = numGen(7) //num.next()
+
+// const iterator = {
+//     [Symbol.iterator](n=10) {
+//         let i = 0
+//         return {
+//             next() {
+//                 if (i < n){
+//                     return { value: ++i, done: false}
+//                 }
+//                 return { value: undefined, done: true}
+//             }
+//         }
+//     }
+// }
+
+// for( let k of iterator) {
+//     console.log(k)
+// }
+
+// for( let l of 'iterator') {
+//     console.log(l)
+// }
+
+function* iter(n=10) {
+    for (let i = 0; i < n; i++){
+        yield i
+    }
+}
+for( let n of iter(6)) {
+        console.log(n)
+}
